91e62ede6826a019f17e1aadf31b2acac2a54317
eltonbear <eltonbear@berkeley.edu>
2017-01-31 20:23:36 -0800

    change properties x to X, y to Y

--- Point.m
+++ Point.m
@@ -3,32 +3,32 @@ classdef Point
     %   Detailed explanation goes here
     
     properties
-        x = 0;
-        y = 0;
+        X = 0;
+        Y = 0;
     end
     
     methods
         function obj = Point(xval,yval)
             if nargin > 0
-                obj.x = xval;
-                obj.y = yval;
+                obj.X = xval;
+                obj.Y = yval;
             end
         end
         
         function d = distance(pt1, pt2)
-            d = sqrt((pt1.x - pt2.x)^2 + (pt1.y - pt2.y)^2);
+            d = sqrt((pt1.X - pt2.X)^2 + (pt1.Y - pt2.Y)^2);
         end
         
         function r = plus(pt1, pt2)
-            r = Point(pt1.x + pt2.x, pt1.y + pt2.y);
+            r = Point(pt1.X + pt2.X, pt1.Y + pt2.Y);
         end
         
         function r = minus(pt1, pt2)
-            r = Point(pt1.x - pt2.x, pt1.y - pt2.y);
+            r = Point(pt1.X - pt2.X, pt1.Y - pt2.Y);
         end
         
         function display(pt)
-            string = [inputname(1) ' = <' num2str(pt.x) ',' num2str(pt.y) '>'];
+            string = [inputname(1) ' = <' num2str(pt.X) ',' num2str(pt.Y) '>'];
             disp(string);
         end
         
