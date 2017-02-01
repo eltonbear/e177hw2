ef0ad854853c3e56178f4b58ef20c92337134468
eltonbear <eltonbear@berkeley.edu>
2017-01-31 20:53:25 -0800

    added properties' classes check using set methods

--- LineSegment.m
+++ LineSegment.m
@@ -13,12 +13,30 @@ classdef LineSegment
             line.Finish = pt2;
         end
         
+        function line = set.Start(line, obj)
+            % why cant this be inplemented in constructor?
+            % what exactly is set.Property
+            if isa(obj, 'Point')
+                line.Start = obj;
+            else
+                error('Properties must be Point objects')
+            end
+        end
+        
+        function line = set.Finish(line, obj)
+            if isa(obj, 'Point')
+                line.Finish = obj;
+            else
+                error('Properties must be Point objects')
+            end
+        end
+        
         function l = length(line)
             l = line.Start.distance(line.Finish);
         end
         
         function display(line)
-            % Does it display when a lineSeg object is initilized 
+            % Does it display when a lineSeg object is initilized? 
             figure;
             plot([line.Start.X, line.Finish.X], [line.Start.Y, line.Finish.Y])
         end
