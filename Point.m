1259e4e7388c455495cbe90a18ccdba94e3273fa
eltonbear <eltonbear@berkeley.edu>
2017-01-31 20:05:25 -0800

    added method 'display'

--- Point.m
+++ Point.m
@@ -26,6 +26,12 @@ classdef Point
         function r = minus(pt1, pt2)
             r = Point(pt1.x - pt2.x, pt1.y - pt2.y);
         end
+        
+        function display(pt)
+            string = [inputname(1) ' = <' num2str(pt.x) ',' num2str(pt.y) '>'];
+            disp(string);
+        end
+        
     end
     
 end
